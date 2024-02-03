5b27e6c873fd4360ae1b31139a7e45c0783ade83
Suyash Shukla <suyashshukla@Suyashs-MacBook-Air.local>
2024-02-03 20:06:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css"
+const phrases = [
+  "No", "Are you sure", "Just say yes" , "Don't do this to me","Bholu say yes", "I'm gonna cry...", "You are breaking my heart ;(", "Last Chance :["
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 45 + 15;
+  const handleNoClick  = () => setNoCount(noCount + 1);
+  const getNoButtonText = () => {
+    return phrases[Math.min(noCount,phrases.length-1)];
+  }
+    return (
+     <div className="valentine-container">
+      {yesPressed ? (
+        <>
+        <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="Bear Kissiing" />
+        </>
+      ):(
+        <>
+        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExa3VxZG9oYjlnNDYwdWZ1b3pub3pyYzF2bGdzaWl1bjFudTBsd2NuZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7zGmpJmwUp5lNFudOf/giphy.gif" alt="Plese" />
+        <div>
+          Will you be my Valentine ?
+        </div>
+        <button
+        className="yesButton"
+        style={{fontSize : yesButtonSize}}
+        onClick = {() => setYesPressed(true)}>
+          Yes
+        </button>
+        <button onClick={handleNoClick} className = "noButton" >
+          {getNoButtonText()}
+        </button>
+        </>
+      )
+      }
+     </div>
+    );
 }
 
 export default App;
